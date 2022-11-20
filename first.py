7ad38ecc5a350670b6d042fe41aa2ca04c47a89a
phakmitl <65010710@kmitl.ac.th>
2022-11-20 22:10:48 +0700

    game4

--- first.py
+++ first.py
@@ -1,21 +1,27 @@
 import random
 import pygame
 import time
+import menu_game
 
 pygame.init()
 
 display_width = 400
 display_height = 800
 
-jump = pygame.mixer.Sound("C:\\Users\\p.mua\\Desktop\\pygame\\jump2.mp3")
+jump = pygame.mixer.Sound("C:\\Users\\p.mua\\Desktop\\pygame\\BGM\\jump2.mp3")
 
 gameDisplay = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption('dang dang')
-clock = pygame.time.Clock()
-dangimg = pygame.image.load("C:\\Users\\p.mua\\Desktop\\pygame\\dangwithred.png")
+clock   = pygame.time.Clock()
+
+dangimg = pygame.image.load("C:\\Users\\p.mua\\Desktop\\pygame\\IMG\\dangwithred.png")
 dangimg = pygame.transform.scale(dangimg, (20, 30))
-black = (0,0,0)
-white = (255,255,255)
+barimg  = pygame.image.load("C:\\Users\\p.mua\\Desktop\\pygame\\IMG\\bar.png")
+barimg  = pygame.transform.scale(barimg, (50, 5))
+
+black   = (0,0,0)
+white   = (255,255,255)
+grey    = (150,150,150)
 
 largeText = pygame.font.Font('Minecraft.ttf',70)
 smallText = pygame.font.Font('Minecraft.ttf',40)
@@ -27,40 +33,78 @@ cy = 0
 pause = False
 
 def player(x,y):
+    global cy
     gameDisplay.blit(dangimg, (x,y))
 
-def bar():
-    global cy
-    for b in range(8):
-        pygame.draw.line(gameDisplay, 0, (barx[b],bary[b]+cy) , ((barx[b]+50),bary[b]+cy))
-
-def text_objects(text, font):
-    textSurface = font.render(text, True, black)
+def text_objects(text, font, color):
+    textSurface = font.render(text, True, color)
     return textSurface, textSurface.get_rect()
 
-
-def message_display(text):
-    largeText = pygame.font.Font('Minecraft.ttf',50)
-    TextSurf, TextRect = text_objects(text, largeText)
-    TextRect.center = ((display_width/2),(display_height/2))
-    gameDisplay.blit(TextSurf, TextRect)
-    pygame.display.update()
-    time.sleep(3)
-    gameloop()
-
-def crash():
-    message_display('GAME OVER')
+def bar():
+    for b in range(8):
+        gameDisplay.blit(barimg, (barx[b],bary[b]+cy))
+#       pygame.draw.line(gameDisplay, 0, (barx[b],bary[b]+cy) , ((barx[b]+50),bary[b]+cy))
 
 def score(count):
     s = int(count)
-    font = pygame.font.SysFont('Minecraft.ttf', 25)
-    text = font.render("Score: "+str(s), True, black)
-    gameDisplay.blit(text,(0,0))
+    font = pygame.font.Font('Minecraft.ttf', 15)
+    sc = font.render("Score: "+str(s), True, black)
+    gameDisplay.blit(sc,(10,10))
     pygame.display.update()
+
+
+def crash():
+    pygame.mixer.music.pause()
+    pygame.mixer.music.load("C:\\Users\\p.mua\\Desktop\\pygame\\BGM\\miss.mp3")
+    pygame.mixer.music.play(1)
+    over = True
+
+    while over:
+        for event in pygame.event.get():
+            (event)
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+                
+        TextSurf, TextRect = text_objects("GAME OVER", menuText, black)
+        TextRect.center = (200,200)
+        gameDisplay.blit(TextSurf, TextRect)
+
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+
+        if 250 > mouse[0] > 150 and 425 > mouse[1] > 325:
+            TextSurf, TextRect = text_objects("Play again", smallText, grey)
+            TextRect.center = (200,400)
+            gameDisplay.blit(TextSurf, TextRect)
+            if click[0] == 1:
+                over = False
+                gameloop()
+        else:
+            TextSurf, TextRect = text_objects("Play again", smallText, black)
+            TextRect.center = (200,400)
+            gameDisplay.blit(TextSurf, TextRect)
+
+
+        if 250 > mouse[0] > 150 and 525 > mouse[1] > 425:
+            TextSurf, TextRect = text_objects("menu", smallText, grey)
+            TextRect.center = (200,500)
+            gameDisplay.blit(TextSurf, TextRect)
+            if click[0] == 1:
+                over = False
+                game_intro()
+        else:
+            TextSurf, TextRect = text_objects("menu", smallText, black)
+            TextRect.center = (200,500)
+            gameDisplay.blit(TextSurf, TextRect)
+
+        pygame.display.update()
+        clock.tick(60)
+        
 
 def game_intro():
     pygame.mixer.music.pause()
-    pygame.mixer.music.load("C:\\Users\\p.mua\\Desktop\\pygame\\menu.mp3")
+    pygame.mixer.music.load("C:\\Users\\p.mua\\Desktop\\pygame\\BGM\\menu.mp3")
     pygame.mixer.music.play(-1)
     intro = True
     ychange = 1
@@ -68,26 +112,52 @@ def game_intro():
 
     while intro:
         for event in pygame.event.get():
-            print(event)
+
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
                 
         gameDisplay.fill(white)
 
-        TextSurf, TextRect = text_objects("Dang", largeText)
-        TextRect.center = ((200),(120))
+        TextSurf, TextRect = text_objects("Dang", largeText, black)
+        TextRect.center = (200),(120)
         gameDisplay.blit(TextSurf, TextRect)
 
-        TextSurf, TextRect = text_objects("Dang", largeText)
-        TextRect.center = ((200),(200))
+        TextSurf, TextRect = text_objects("Dang", largeText, black)
+        TextRect.center = (200),(200)
         gameDisplay.blit(TextSurf, TextRect)
 
-        TextSurf, TextRect = text_objects("Adventure", smallText)
-        TextRect.center = ((200),(270))
+        TextSurf, TextRect = text_objects("Adventure", smallText, black)
+        TextRect.center = (200),(270)
         gameDisplay.blit(TextSurf, TextRect)
 
-        button("Play",150, 250, 325, 425, 200, 400)
+        #button("Play",150, 250, 325, 425, 200, 400)
+
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+
+        if 250 > mouse[0] > 150 and 425 > mouse[1] > 325:
+            TextSurf, TextRect = text_objects("Play", menuText, black)
+            TextRect.center = (200,380)
+            gameDisplay.blit(TextSurf, TextRect)
+            if click[0] == 1:
+                    gameloop()
+        else:
+            TextSurf, TextRect = text_objects("Play", smallText, black)
+            TextRect.center = (200,380)
+            gameDisplay.blit(TextSurf, TextRect)
+
+        if 250 > mouse[0] > 150 and 525 > mouse[1] > 425:
+            TextSurf, TextRect = text_objects("Board", menuText, black)
+            TextRect.center = (200,480)
+            gameDisplay.blit(TextSurf, TextRect)
+            if click[0] == 1:
+                    pass
+        else:
+            TextSurf, TextRect = text_objects("Board", smallText, black)
+            TextRect.center = (200,480)
+            gameDisplay.blit(TextSurf, TextRect)
+
 
         player(190, y)
                 
@@ -100,8 +170,8 @@ def game_intro():
 
         pygame.display.update()
         clock.tick(60)
-
-
+        
+'''
 def button(msg,x1,x2,y1,y2, posx, posy):
     mouse = pygame.mouse.get_pos()
     click = pygame.mouse.get_pressed()
@@ -116,19 +186,13 @@ def button(msg,x1,x2,y1,y2, posx, posy):
         TextSurf, TextRect = text_objects(msg, smallText)
         TextRect.center = (posx,posy)
         gameDisplay.blit(TextSurf, TextRect)
+'''
+
 
 def paused():
     pygame.mixer.music.pause()
-    TextSurf, TextRect = text_objects("Paused", largeText)
+    TextSurf, TextRect = text_objects("Paused", largeText, black)
     TextRect.center = (200,120)
-    gameDisplay.blit(TextSurf, TextRect)
-
-    TextSurf, TextRect = text_objects("continue", menuText)
-    TextRect.center = (200,400)
-    gameDisplay.blit(TextSurf, TextRect)
-
-    TextSurf, TextRect = text_objects("menu", menuText)
-    TextRect.center = (200,500)
     gameDisplay.blit(TextSurf, TextRect)
 
     pause = True
@@ -144,13 +208,29 @@ def paused():
         click = pygame.mouse.get_pressed()
 
         if 250 > mouse[0] > 150 and 425 > mouse[1] > 325:
+            TextSurf, TextRect = text_objects("continue", smallText, grey)
+            TextRect.center = (200,400)
+            gameDisplay.blit(TextSurf, TextRect)
             if click[0] == 1:
                 pause = False
 
-        elif 250 > mouse[0] > 150 and 525 > mouse[1] > 425:
+        else:
+            TextSurf, TextRect = text_objects("continue", smallText, black)
+            TextRect.center = (200,400)
+            gameDisplay.blit(TextSurf, TextRect)
+
+
+        if 250 > mouse[0] > 150 and 525 > mouse[1] > 425:
+            TextSurf, TextRect = text_objects("menu", smallText, grey)
+            TextRect.center = (200,500)
+            gameDisplay.blit(TextSurf, TextRect)
             if click[0] == 1:
                 pause = False
                 game_intro()
+        else:
+            TextSurf, TextRect = text_objects("menu", smallText, black)
+            TextRect.center = (200,500)
+            gameDisplay.blit(TextSurf, TextRect)
 
         pygame.display.update()
         clock.tick(15)  
@@ -175,7 +255,7 @@ def gameloop():
         barx[b] = xbar
     
     pygame.mixer.music.pause()
-    pygame.mixer.music.load("C:\\Users\\p.mua\\Desktop\\pygame\\play.mp3")
+    pygame.mixer.music.load("C:\\Users\\p.mua\\Desktop\\pygame\\BGM\\play.mp3")
     pygame.mixer.music.play(-1)
 
     while not crashed:
@@ -186,10 +266,11 @@ def gameloop():
                 quit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    xchange = - 3
+                    xchange = - 2.5
                 elif event.key == pygame.K_RIGHT:
-                    xchange =  3
+                    xchange =  2.5
                 elif event.key == pygame.K_UP and doublejump == True:
+                    pygame.mixer.Sound.play(jump)
                     ychange = -3
                     doublejump = False
                 elif event.key == pygame.K_p:
@@ -208,10 +289,10 @@ def gameloop():
                 y += 0.5
         if y > 300:
             y = y + ychange
-            
-        x = x + xchange    
+                
         ychange += 0.08
-
+        x = x + xchange
+        
         #ขอบเช็ค    
         if x > 380:
             xchange = 0
@@ -225,9 +306,9 @@ def gameloop():
             
         #เช็คbarhit
         for i in range(8):
-            if y+30 <= bary[i]+4+cy and y+30 >= bary[i]-4+cy:
+            if x+15 > barx[i] and x <= barx[i]+45 and ychange > 0:
                 z += 1
-                if x+15 >= barx[i] and x <= barx[i]+45:
+                if y+30 <= bary[i]+4+cy and y+30 >= bary[i]-4+cy:
                     ychange = -3.5
                     doublejump = True
                     print(i+1)
--- test.py
+++ test.py
@@ -1,7 +1,3 @@
-import random
-import pygame
-import time
-
-pygame.init()
-
-print(pygame.font.get_fonts())
+def cat():
+    a = 10
+    return a
